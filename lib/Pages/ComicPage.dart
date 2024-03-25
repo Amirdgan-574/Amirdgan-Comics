d584e84a6748bfebdd976677c452e83102598b3b
Amirdgan-574 <159114812+Amirdgan-574@users.noreply.github.com>
2024-03-25 13:25:12 +0300

    Update ComicPage.dart

--- lib/Pages/ComicPage.dart
+++ lib/Pages/ComicPage.dart
@@ -15,7 +15,59 @@ class ComicPage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,import 'package:flutter/material.dart';
+import 'package:amirmagomedov/Pages/Comic.dart';
+
+class ComicPage extends StatelessWidget {
+  final Comic comic;
+
+  ComicPage(this.comic);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(comic.title, style: TextStyle(color: Colors.black)),
+      ),
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: ListView(
+                padding: const EdgeInsets.all(8),
+                scrollDirection: Axis.vertical,
+                children: <Widget>[
+                  Text(comic.description),
+                  SizedBox(height: 20),
+                  Text(comic.price),
+                  SizedBox(height: 10),
+                  Container(
+                    height: 600,
+                    child: PageView.builder(
+                      itemCount: comic.images.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.network(
+                            comic.images[index],
+                            fit: BoxFit.cover,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
           children: <Widget>[
             Text(
               comic.title,
