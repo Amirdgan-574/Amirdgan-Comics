e40577d8efe6d5b54167f92050de5ffe5ef1b1f8
Amirdgan-574 <159114812+Amirdgan-574@users.noreply.github.com>
2024-03-25 13:24:52 +0300

    Update Comic.dart

--- lib/Pages/Comic.dart
+++ lib/Pages/Comic.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 class Comic {
   final String title;
   final String description;
+  final String price;
   final List<String> images;
-
-  Comic({required this.title, required this.description, required this.images});
+  Comic({required this.title, required this.description, required this.images,required this.price});
 }
